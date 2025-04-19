6f0f7278a37f82ee30aee5264407587fe364b32a
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-04-19 18:14:49 +0200

    drawing a chart to visualize the coovariance between the 5 feature in a matrix

--- linearRegression.py
+++ linearRegression.py
@@ -20,7 +20,15 @@ print(df.isnull().sum())
 
 import matplotlib.pyplot as plt
 from mlxtend.plotting import scatterplotmatrix
-scatterplotmatrix(df.values, figsize=(12, 10), names=df.columns, alpha=0.5)
+# scatterplotmatrix(df.values, figsize=(12, 10), names=df.columns, alpha=0.5)
 
+# plt.tight_layout()
+# plt.show()
+
+import numpy as np
+from mlxtend.plotting import heatmap
+
+cm = np.correcoef(df.values.T)
+hm = heatmap(cm, row_names=df.columns, column_names=df.columns)
 plt.tight_layout()
 plt.show()
