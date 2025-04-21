97d98a845cd12428541b6dbbe233b1b2d3f65292
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-04-21 15:39:17 +0200

    obtaining the inliers and outliers from the fitted RANSAC linear regression model

--- ransacregressor.py
+++ ransacregressor.py
@@ -32,5 +32,20 @@ ransac = RANSACRegressor(
     residual_threshold=None, # default value
     random_state=123
 )
-
+import matplotlib.pyplot as plt
+from mlxtend.plotting import scatterplotmatrix
+import numpy as np
 ransac.fit(X, y)
+
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+line_X = np.arange(3, 10, 1)
+line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+plt.scatter(X[inlier_mask], y[inlier_mask], c='steelblue', edgecolor='white', marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask], c='limegreen', edgecolor='white', marker='s', label='Outliers')
+plt.plot(line_X, line_y_ransac, color='black', lw=2)
+plt.xlabel('Living area above ground in square feet')
+plt.ylabel('Sale price in U.S. dollars')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
