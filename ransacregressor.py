ad2ce4fe5fc8f3c6ba47ed00a8a394bd56bc339b
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-04-22 12:02:36 +0200

    working with polynomial

--- ransacregressor.py
+++ ransacregressor.py
@@ -49,3 +49,142 @@ plt.ylabel('Sale price in U.S. dollars')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
+
+print(f'Slope: {ransac.estimator_.coef_[0]:.3f}')
+print(f'Intercept: {ransac.estimator_.intercept_:.3f}')
+
+def median_absolute_deviation(data):
+    return np.median(np.abs(data - np.median(data)))
+
+median_absolute_deviation(y)
+
+from sklearn.model_selection import train_test_split
+target = 'SalePrice'
+features = df.columns[df.columns != target]
+X = df[features].values
+y = df[target].values
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=123
+)
+
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+
+x_max = np.max([np.max(y_train_pred), np.max(y_test_pred)])
+x_min = np.min([np.min(y_train_pred), np.min(y_test_pred)])
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(7, 3), sharey  = True)
+
+ax1.scatter( 
+    y_test_pred, y_test_pred - y_test, c='limegreen', marker='s', edgecolor='white', label='Test data'
+)
+
+ax2.scatter(
+    y_train_pred, y_train_pred -y_train, c='steelblue', marker='o', edgecolor='white', label='Training data'
+)
+
+ax1.set_ylabel('Residuals')
+
+for ax in (ax1, ax2):
+    ax.set_xlabel('Predicted values')
+    ax.legend(loc='upper left')
+    ax.hlines(y=0, xmin=x_min-100, xmax=x_max+100, color='black', lw=2)
+    
+plt.tight_layout()
+plt.show()
+
+from sklearn.metrics import mean_squared_error
+mse_train = mean_squared_error(y_train, y_train_pred)
+mse_test = mean_squared_error(y_test, y_test_pred)
+print(f'MSE train: {mse_train:.2f}')
+print(f'MSE test: {mse_test:.2f}')
+
+# computing the mean absolute error MSE
+from sklearn.metrics import mean_absolute_error
+mae_train = mean_absolute_error(y_train, y_train_pred)
+mae_test = mean_absolute_error(y_test, y_test_pred)
+print(f'MAE train: {mae_train:.2f}')
+print(f'MAE test: {mae_test:.2f}')
+
+from sklearn.metrics import r2_score
+train_r2 = r2_score(y_train, y_train_pred)
+test_r2 = r2_score(y_test, y_test_pred)
+print(f'R^2 train: {train_r2:.3f}, {test_r2:.3f}')
+
+from sklearn.linear_model import Ridge
+ridge = Ridge(alpha=1.0)
+from sklearn.linear_model import Lasso
+lasso = Lasso(alpha=1.0)
+from sklearn.linear_model import ElasticNet
+elanet = ElasticNet(alpha=1.0, l1_ratio=0.5)
+
+# Add a  second-degree polynomial term
+from sklearn.preprocessing import PolynomialFeatures
+X = np.array([ 258.0, 270.0, 294.0, 320.0, 342.0, 368.0, 396.0, 446.0, 480.0, 586.0])[:, np.newaxis]
+y = np.array([ 236.4, 234.4, 252.8, 298.6, 314.2, 342.2, 360.8, 368.0, 391.2, 390.8])
+
+lr = LinearRegression()
+pr = LinearRegression()
+quadratic = PolynomialFeatures(degree = 2)
+X_quad = quadratic.fit_transform(X)
+lr.fit(X, y)
+X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+y_lin_fit = lr.predict(X_fit)
+
+pr.fit(X_quad, y)
+y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+plt.scatter(X, y, label='Training points')
+plt.plot(X_fit, y_lin_fit, label='Linear fit', linestyle='--')
+plt.plot(X_fit, y_quad_fit, label='Quadratic fit')
+plt.xlabel('Explanatory variable')
+plt.ylabel('Predicted or known target values')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+y_lin_pred  =lr.predict(X)
+y_quad_pred = pr.predict(X_quad)
+mse_lin = mean_squared_error(y, y_lin_pred)
+mse_quad = mean_squared_error(y, y_quad_pred)
+print(f'Training MSE linear: {mse_lin:.3f}, quadratic: {mse_quad:.3f}')
+r2_lin = r2_score(y, y_lin_pred)
+r2_quad = r2_score(y, y_quad_pred)
+print(f'Training R^2 linear: {r2_lin:.3f}, quadratic: {r2_quad:.3f}')
+
+X = df[['Gr Liv Area']].values
+y = df['SalePrice'].values
+X = X[(df['Gr Liv Area'] < 4000)]
+y = y[(df['Gr Liv Area'] < 4000)]
+
+regr = LinearRegression()
+# create a quadratic and cubic features
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = quadratic.fit_transform(X)
+
+# fit to features
+X_fit = np.arange(X.min()-1, X.max()+2, 1)[:, np.newaxis]
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2  = r2_score(y, regr.predict(X))
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+
+# plot the results
+plt.scatter(X, y, label='Training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label=f'Linear (d=1), $R^2$={linear_r2:.2f}', color='blue', lw=2, linestyle=':')
+
+plt.plot(X_fit, y_quad_fit, label=f'Quadratic (d=2), $R^2$={quadratic_r2:.2f}', color='red', lw=2, linestyle='-')
+plt.plot(X_fit, y_cubic_fit, label=f'Cubic (d=3), $R^2$={cubic_r2:.2f}', color='green', lw=2, linestyle='--')
+plt.xlabel('Living area above ground in sqare feet')
+plt.legend(loc='upper left')
+plt.show()
